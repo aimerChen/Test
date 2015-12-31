83daceb501289ae00d6c12b0cbbd92ebb88af205
chen <chen@chen-pc>
2015-12-31 22:44:05 +0800

    commit

--- Test/build/classes/user.sql
+++ Test/build/classes/user.sql
@@ -11,19 +11,28 @@ create table user(
 	primary key(id)
 )DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci engine=InnoDB;
 
-drop table if exists roles;
-create table roles(
+drop table if exists role;
+create table role(
 	id int(11) auto_increment,
 	name varchar(128) unique not null,
 	createTime timestamp,
-	priority varchar(1),
+	updateTime timestamp,
 	primary key(id)
 )DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci engine=InnoDB;
 
-drop table if exists rolesuser;
-create table rolesuser(
+drop table if exists path;
+create table path(
+	id int(11) auto_increment,
+	name varchar(256)  not null,
+	role_id int(11) not null,
+	createTime timestamp,
+	PRIMARY KEY (id) 
+)DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci engine=InnoDB;
+
+drop table if exists roleuser;
+create table roleuser(
 	id int(11) auto_increment,
 	userid int(11) ,
-	rolesid int(11),
+	roleid int(11),
 	PRIMARY KEY (id) 
 )DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci engine=InnoDB;
